b229b658b8b9a648aecfcb743a71029a3427750a
Ibtesham Akhtar <ibteshamakhtar1@gmail.com>
2023-11-14 23:03:21 +0530

    First Commit

---
+++ file1.js
@@ -0,0 +1,33 @@
+// Data types
+
+//Primitive
+
+// 7 types: String , Numbers, Boolean, Null, Undefined, Stmbol, BigInt
+
+const score =100
+const scoreValue= 100.3
+const isLoggedin = false
+const outSide = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = ('123')
+console
+
+//Reference Type(Non Primitive)
+
+//Array, Objects , Functions
+
+const hroes = ["Saktiman", "Spidermen","Supermen"]
+
+let myObj = {
+    name: "Ibtesham",
+    phoneNumber: 897484,
+    age: 23
+
+}
+console.log(myObj);
+
+const myFunction = function(){
+    console.log("Hello");
+}
+console.log(myFunction);
